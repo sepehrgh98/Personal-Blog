d6440ae2418acf18bc758824e0f536b7afca3567
sepehrgh98 <sepehrghamri@gmail.com>
2021-01-27 20:40:47 +0330

    models added

--- final_project/blog/admin.py
+++ final_project/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Post_state, Comment, Post_content, Post_category
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Post_state)
+admin.site.register(Comment)
+admin.site.register(Post_content)
+admin.site.register(Post_category)
+
--- final_project/blog/models.py
+++ final_project/blog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
+    post_date = models.DateTimeField(default=timezone.now)
+    last_update = models.DateTimeField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    state = models.ForeignKey('Post_state', on_delete=models.CASCADE)
+    tag = models.ManyToManyField('Tag', blank=True)
+    content = models.OneToOneField('Post_content', on_delete=models.CASCADE)
+    category = models.ForeignKey('Post_category', on_delete=models.CASCADE)
+    comments = models.ManyToManyField('Comment', blank=True)
+
+
+
+class Post_state(models.Model):
+    class Meta:
+        verbose_name = 'لایک'
+        verbose_name_plural = 'لایک ها'
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    state = models.BooleanField()
+    state_date = models.DateTimeField(default=timezone.now)
+    last_update = models.DateTimeField()
+
+class Comment(models.Model):
+    class Meta:
+        verbose_name = 'نظر'
+        verbose_name_plural = 'نظرات'
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    last_update = models.DateTimeField()
+    content = models.CharField(max_length=1000)
+    comment_date = models.DateTimeField(default=timezone.now)
+
+
+
+
+class Tag(models.Model):
+    class Meta:
+        verbose_name = 'برچسب'
+        verbose_name_plural = 'برچسب ها'
+    name = models.CharField(max_length=200)
+    last_update = models.DateTimeField()
+
+
+class Post_content(models.Model):
+    class Meta:
+        verbose_name = 'محتوا'
+        verbose_name_plural = 'محتوا ها'
+    title = models.CharField(max_length=500)
+    text = models.TextField()
+    image = models.URLField()
+    last_update = models.DateTimeField()
+
+class Post_category(models.Model):
+    class Meta:
+        verbose_name = 'گروه بندی پست ها'
+        verbose_name_plural = 'گروه بندی های پست ها'
+    name = models.CharField(max_length=200)
+    last_update = models.DateTimeField()
+
+
+
